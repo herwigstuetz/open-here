462619d442592d0a5dffe8c413beea1549c9445e
Herwig Stuetz <herwig@herwigstuetz.com>
2021-02-14 18:31:27 +0000

    Read server config from environment

--- src/client.rs
+++ src/client.rs
@@ -6,16 +6,30 @@ use crate::cmd;
 use reqwest::Client;
 use tokio::*;
 
+
+use std::env;
+use envconfig::Envconfig;
+
+#[derive(Envconfig)]
+struct Config {
+    #[envconfig(from = "OH_HOST")]
+    pub oh_host: String,
+
+    #[envconfig(from = "OH_PORT")]
+    pub oh_port: Option<u16>,
+}
+
+
 struct OpenClient {
     client: Client,
     server: String,
 }
 
 impl OpenClient {
-    fn new() -> Self {
+    fn new(server: String) -> Self {
         Self {
             client: Client::new(),
-            server: std::string::String::from("http://127.0.0.1:8010"),
+            server,
         }
     }
 
@@ -33,8 +47,10 @@ impl OpenClient {
 
 #[tokio::main]
 pub async fn open(open: cli::OpenTarget) {
+    let cfg = Config::init().unwrap();
+    let server = format!("http://{}:{}", cfg.oh_host, cfg.oh_port.unwrap_or(8080));
 
-    let client = OpenClient::new();
+    let client = OpenClient::new(server);
 
     match client.open(open).await {
         Ok(_) => {},
