60bdf55e1e1641dcfe2ab6da809f9c7ff266def7
Herwig Stuetz <herwig@herwigstuetz.com>
2021-03-10 21:44:36 +0000

    Add test

---
+++ tests/open_check.rs
@@ -0,0 +1,39 @@
+//! tests/open_check.rs
+
+use open_here::cli;
+use open_here::client;
+use open_here::open;
+use open_here::server;
+use open_here::setup_logger;
+
+use std::thread;
+
+#[test]
+fn server_test() {
+    setup_logger(4);
+
+    let server = server::Server::new(server::Config {
+        host: "localhost:0".to_string(),
+        dry_run: true, // we do not actually want to open something
+    })
+    .unwrap();
+
+    let port = server.get_port().unwrap();
+
+    let _server_thread = thread::spawn(|| server.run());
+
+    let target = "http://localhost:1234".to_string();
+
+    let res = open(
+            client::Config {
+                host: format!("localhost:{}", port)
+            },
+            cli::OpenTarget {
+                target: target.clone()
+            }
+        ).unwrap();
+
+    assert!(
+        res.contains("Would run:") && res.contains(&target)
+    );
+}
